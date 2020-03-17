229962dcdefa082b125b317c340afd3ce7199c6a
Arghun-dev <arghun.developer@gmail.com>
2020-03-17 16:00:13 +0330

    Created Navbar Component and some styling

---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar">
+        <nav class="blue darken-4">
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Ninja Smoothies</a>
+                <div class="nav-content">
+                    <a class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+                        <i class="material-icons red">add</i>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.navbar nav {
+    padding: 0 20px;
+}
+</style>
